1b252bf8a5a7d4d7a12acc2d7c7572a96ab108ac
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-01-10 14:34:25 +0200

    Update project constants

--- project/constants.py
+++ project/constants.py
@@ -1,5 +1,4 @@
 """Konstantes kuras tiek izmantotas 'project' aplikācijā"""
 
 WRONG_PROJECT_NAME = "Projekta nosaukumam jāsastāv no burtiem un/vai cipariem"
-PROJECT_EXISTS = "Projekts ar doto nosaukumu jau eksistē"
-USEXPECTED_ERROR_MSG = "Unexpected exception occured"
+PROJECT_EXISTS_MSG = "Projekts ar doto nosaukumu jau eksistē"
