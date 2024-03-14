7d81d438e27bbcfc3736e27ba63b188a72c03861
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-03-14 12:37:05 +0200

    Delete redundant constant

--- project/constants.py
+++ project/constants.py
@@ -1,4 +1,3 @@
 """Konstantes kuras tiek izmantotas 'project' aplikācijā"""
 
-WRONG_PROJECT_NAME = "Projekta nosaukumam jāsastāv no burtiem un/vai cipariem"
 PROJECT_EXISTS_MSG = "Projekts ar doto nosaukumu jau eksistē"
