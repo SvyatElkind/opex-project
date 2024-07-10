187126dd1c989b1e92f1d0fa247f3ecf765ad9f0
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-07-10 11:19:58 +0300

    Add new constants

--- project/helpers/constants.py
+++ project/helpers/constants.py
@@ -3,4 +3,11 @@
 PROJECT_EXISTS_MSG = "Projekts ar doto nosaukumu jau eksistē"
 
 # CharFields max length of Project model
-PROJECT_NAME_LENGTH = 50
+PROJECT_NAME_LENGTH = 20
+
+# Rgular expressions
+REGEX_PROJECT_NAME = r'^[\w\-]+$'
+
+# Error messages
+WRONG_PROJECT_NAME_LENGTH = f'Projekta nosaukums nevar būt garāks par {PROJECT_NAME_LENGTH} simboliem'
+WRONG_PROJECT_NAME_SYMBOLS = 'Projekta nosaukumā var izmantot burtus, ciparus "_" un "-".'
