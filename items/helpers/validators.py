c34679659456a3bf108eb38ebc506b6bbc7b9db9
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-07-17 14:49:54 +0300

    refactor validation functions

--- items/helpers/validators.py
+++ items/helpers/validators.py
@@ -1,11 +1,48 @@
 """Module contains validation functions for Item class."""
 
-import re
-from inspect import signature
 
-from helpers.constants import NO_VALUE
-from inventories.models import Inventory
-from items.helpers.constants import ITEM_ANNOTATION_LENGTH, ITEM_DATE_NOTE_LENGTH, ITEM_NOTES_LENGTH, ITEM_NUMBER_ERROR_MSG, ITEM_SERIES_CODE_ERROR_MSG, ITEM_SERIES_CODE_LENGTH, ITEM_SISTEMATISATION_LENGTH, ITEM_TITLE_LENGTH, NOT_A_STRING_ERROR_MSG, TOO_LONG_VALUE_ERROR_MSG
+from django.core.exceptions import ValidationError
+
+
+from helpers.constants import (
+    ITEM_RESTRICTION_LIST,
+    ITEM_SECURITY_LEVEL_LIST,
+    NO_VALUE,
+    UNIT_OF_MEASURE_E,
+    UNIT_OF_MEASURE_P
+)
+from items.helpers.constants import (
+    COLOR_FIELD_VALUES,
+    ITEM_ANNOTATION_DEFAULT_VALUE,
+    ITEM_DATE_DEFAULT_VALUE,
+    ITEM_DURATION_DEFULT_VALUE,
+    ITEM_FORMAT_DEFULT_VALUE,
+    ITEM_LANGUAGE_DEFAULT_VALUE,
+    ITEM_RESOLUTION_DEFULT_VALUE,
+    ITEM_RESTRICTION_DEFAULT_VALUE,
+    MSG_E_ITEM_ANNOTATION_REQUIRED,
+    MSG_E_ITEM_COLOR_REQUIRED,
+    MSG_E_ITEM_DATE_VALUE,
+    MSG_E_ITEM_DATE_VALUE_DEFAULT,
+    MSG_E_ITEM_DOES_NOT_EXIST,
+    MSG_E_ITEM_DURATION_REQUIRED,
+    MSG_E_ITEM_FORMAT_REQUIRED,
+    MSG_E_ITEM_LANGUAGE_REQUIRED,
+    MSG_E_ITEM_NUMBER,
+    MSG_E_ITEM_RESOLUTION_REQUIRED,
+    MSG_E_ITEM_SELF_RELATE,
+    MSG_E_ITEM_SIZE_VALUE_FOR_PAPER,
+    MSG_E_RESTRICTION_VALUE,
+    MSG_E_SECUTIRY_LEVEL_VALUE,
+    MSG_E_UNIT_OF_MEASURE_VALUE,
+    NOT_REQUIERE_LANGUAGE_TYPE,
+    RELATED_ITEM,
+    REQUIERE_ANNOTATION_TYPE,
+    REQUIERE_COLOR_TYPE,
+    REQUIERE_DURATION_TYPE,
+    REQUIERE_FORMAT_TYPE,
+    REQUIERE_RESLOLUTION_TYPE
+)
 
 
 def is_consecutive(items: list) -> bool:
@@ -31,261 +68,289 @@ def is_consecutive(items: list) -> bool:
             
     return True
 
-def check_char_field_length(value, allowed_length) -> None | str:
-    """Checks char field length.
+
+# Field validation functions:
+# validate_item_security_level
+# validate_item_restriction
+def validate_item_security_level(security_level: str):
+    """Validate item security level.
+
+    This is field validation function.
     
     Args:
-        value: Value to be validated.
-        allowed_length: Allowed char field max_length
+        security_level: Item security_level.
+    
+    Raises:
+        ValidationError: If security_level value is not one of allowed values.
+    """
+    if not security_level in ITEM_SECURITY_LEVEL_LIST:
+        raise ValidationError(MSG_E_SECUTIRY_LEVEL_VALUE)
+    
+
+def validate_item_restriction(restriction: str) -> None:
+    """Validate item restriction.
+
+    This is field validation function.
+    
+    Args:
+        restriction: Item restriction.
+    
+    Raises:
+        ValidationError: If restriction value is not one of allowed values.
+    """
+    if not restriction in ITEM_RESTRICTION_LIST:
+        raise ValidationError(MSG_E_RESTRICTION_VALUE)
+
+
+def validate_related_item(related_items: list[int], item) -> None:
+    """Validate related items.
+    
+    Args:
+        related_items: list of related item id.
+        item: Item instance to which related objects should refere to.
         
-    Returns:
-        None if value is valid else returns error message.
-    """
-   
-    if not isinstance(value, str):
-        return NOT_A_STRING_ERROR_MSG
-    
-    if len(value) > allowed_length:
-        return TOO_LONG_VALUE_ERROR_MSG.format(allowed_length)
+    Raises:
+        ValidationError: If related items contains errors."""
+    # Check if item id exists in given inventory.
+    for id in related_items:
+        if not item.inventory.items.filter(id=id).exists():
+            raise ValidationError({RELATED_ITEM: MSG_E_ITEM_DOES_NOT_EXIST.format(id)}) 
+        
+        # Check if item is relating to self
+        if id == item.id:
+            raise ValidationError({RELATED_ITEM: MSG_E_ITEM_SELF_RELATE}) 
 
-def validate_item_series_code(series_code: str) -> None | str:
-    """Validate item series code.
-
-    Args:
-        series_code: Series code to be validated.
-    
-    Returns:
-        None if series code is valid else returns error message.
-    """
-    result =  check_char_field_length(series_code, ITEM_SERIES_CODE_LENGTH)
-    if result:
-        return result
-    
-    # Create RegEx matching series code pattern.
-    pattern = re.compile(r'^(?!0\d*$)(\d{1,2}\.)*\d{1,2}$')
-
-    # Check if series code matches pattern.
-    if not bool(pattern.match(series_code)):
-        return ITEM_SERIES_CODE_ERROR_MSG
-
-def validate_item_number(number: int, inventory: Inventory) -> None | str:
+# Below are validation functions used in clean() method
+def validate_item_number(item) -> None | str:
     """Validate item number.
     
     Args:
-        number: Number to be validated.
-        inventory: Related inventory object.
+        item: Item instance.
     
     Returns:
         None if number is valid else returns error message.
     """
     # Get suppose number.
-    first_item_number = inventory.last_gv + 1
+    new_item_number = item.inventory.last_gv + 1
 
     # Check if item number is next number after inventory last_gv.
-    if not number == first_item_number:
-        return ITEM_NUMBER_ERROR_MSG.format(inventory.number, first_item_number)
+    if not item.number == new_item_number:
+        return MSG_E_ITEM_NUMBER
+    
 
-def validate_item_title(title: str) -> None | str:
-    """Validate item title.
-
+def validate_item_date(item) -> None | str:
+    """Validate item start and end date.
+    
+    Start date can't be bigger then end date.
+    Date can't be default date.
+    
     Args:
-        title: Item title.
+        item: Item instance.
     
     Returns:
-        None if title is valid else returns error message.
-    """
-    result =  check_char_field_length(title, ITEM_TITLE_LENGTH)
-    if result:
-        return result
+        None if no error else return error message."""
+    if not item.start_date < item.end_date:
+        return MSG_E_ITEM_DATE_VALUE
+    
+    if item.start_date == ITEM_DATE_DEFAULT_VALUE \
+        or item.end_date == ITEM_DATE_DEFAULT_VALUE:
+        return MSG_E_ITEM_DATE_VALUE_DEFAULT
 
-def validate_item_start_date(start_date):
-    pass
 
-def validate_item_end_date(end_date):
-    pass
-
-def validate_item_date_note(date_note):
-    """Validate item date note.
-
+def validate_item_size(item) -> None | str:
+    """Validate item size according to invenotry type and media.
+    
     Args:
-        date_note: Item date note to be validated.
+        item: Item instance.
     
     Returns:
-        None if date note is valid else returns error message.
+        None if item size is correct else returns error message.
     """
-    result =  check_char_field_length(date_note, ITEM_DATE_NOTE_LENGTH)
-    if result:
-        return result
+    if not item.inventory.electronic:
+        # Convert number to string
+        number_str = str(item.item_size)
+        # Split on the decimal point
+        _, fractional_part = number_str.split('.') 
+        # TODO check if at this point i can pass number that is longer then 5 digits
+        # Check if fractional part has any non-zero digit
+        if any(char != '0' for char in fractional_part):
+            return MSG_E_ITEM_SIZE_VALUE_FOR_PAPER
+
+
+def validate_item_unit_of_measure(item) -> None | str:
+    """Validate unit of measure according to inventory media.
     
-def validate_item_size(size: int):
-    pass
-    # TODO Check if item is with decimal part
+    Args:
+        item: Item instance.
 
-def validate_item_unit_of_measur(measure: str):
-    pass
+    Returns:
+        None if measure is valide else returns error message.
+    """
+    if item.inventory.electronic and not item.unit_of_measure in UNIT_OF_MEASURE_E:
+        return MSG_E_UNIT_OF_MEASURE_VALUE
+    elif not item.inventory.electronic and not item.unit_of_measure in UNIT_OF_MEASURE_P:
+        return MSG_E_UNIT_OF_MEASURE_VALUE
+        
 
-def validate_item_date(start_date, end_date):
-    """Validate item start and end date"""
-    if not start_date < end_date:
-        return False
-
-def validate_related_item(related_item: dict):
-    """Validates dict of related items"""
-    pass
-
-def validate_item_notes(notes: str) -> None | str:
-    """Validate item notes.
+def validate_item_language(item) -> None | str:
+    """Validate item language.
+    
+    Language should be mentioned if invenotry type is other then foto.
 
     Args:
-        notes: Item notes to be validated.
+        item: Item instance.
     
     Returns:
-        None if notes is valid else returns error message.
+        None if no errors else return error message.
     """
-    result =  check_char_field_length(notes, ITEM_NOTES_LENGTH)
-    if result:
-        return result
+    if not item.inventory.type == NOT_REQUIERE_LANGUAGE_TYPE \
+        and item.language == ITEM_LANGUAGE_DEFAULT_VALUE:
+        return MSG_E_ITEM_LANGUAGE_REQUIRED
 
-def validate_item_annotation(annotation: str) -> None | str:
-    """Validate item annotation.
+
+def validate_item_restriction_note(item) -> None | str:
+    """Validate item restriction note.
+
+    Item restriction note should not be empty when restriction is
+    diferent from default value.
+    
+    Args:
+        item: Item instance.
+    
+    Returns:
+       None if no errors else error message.
+    """
+    if not item.restriction == ITEM_RESTRICTION_DEFAULT_VALUE \
+       and not item.restriction_note:
+        return MSG_E_RESTRICTION_VALUE
+    
+
+def validate_item_annotation(item) -> None | str:
+    """Validate item annotaion.
+    
+    Annotation is required for specific inventory type.
+    
+    Args:
+        item: Item instance.
+    
+    Returns:
+        None if item annotation is correct else returns error message.
+    """
+    if item.inventory.type in REQUIERE_ANNOTATION_TYPE \
+        and item.annotation == ITEM_ANNOTATION_DEFAULT_VALUE:
+        return MSG_E_ITEM_ANNOTATION_REQUIRED
+
+
+def validate_item_color(item) -> None | str:
+    """Validate item color.
+
+    Color is required for specific inventory type.
+    
+    Args:
+        item: Item instance.
+    
+    Returns:
+        Error message if item color is not one of allowed values
+        else returns None.
+    """
+    if item.inventory.type in REQUIERE_COLOR_TYPE \
+        and not item.color in COLOR_FIELD_VALUES:
+        return MSG_E_ITEM_COLOR_REQUIRED
+
+
+def validate_item_format(item) -> None | str:
+    """Validate item format.
+    
+    Format required for specific inventory type.
 
     Args:
-        annotation: Item annotation to be validated.
+        item: Item instance.
     
     Returns:
-        None if annotation is valid else returns error message.
+        Error message if format is not provided else returns None.
     """
-    result =  check_char_field_length(annotation, ITEM_ANNOTATION_LENGTH)
-    if result:
-        return result
+    if item.inventory.type in REQUIERE_FORMAT_TYPE \
+        and item.format == ITEM_FORMAT_DEFULT_VALUE:    
+        return MSG_E_ITEM_FORMAT_REQUIRED
 
-def validate_item_sistematisation(sistematisation: str):
-    """Validate item sistematisation.
+
+def validate_item_resolution(item) -> None | str:
+    """Validate item resolution.
+    
+    Resolution required for specific inventory type.
 
     Args:
-        sistematisation: Item sistematisation to be validated.
+        item: Item instance.
     
     Returns:
-        None if sistematisation is valid else returns error message.
+        Error message if resolution is not provided else returns None.
     """
-    result =  check_char_field_length(sistematisation, ITEM_SISTEMATISATION_LENGTH)
-    if result:
-        return result
+    if item.inventory.type in REQUIERE_RESLOLUTION_TYPE \
+        and item.resolution == ITEM_RESOLUTION_DEFULT_VALUE:
+        return MSG_E_ITEM_RESOLUTION_REQUIRED
 
-def validate_item_physical_description(description: str):
-    pass
 
-def validate_item_language(language: str):
-    pass
+def validate_item_duration(item) -> None | str:
+    """Validate item duration
+    
+    Duration required for specific inventory type.
 
-def validate_item_restriction(restriction: str):
-    pass
+    Args:
+        item: Item instance.
+    
+    Returns:
+        Error message if duration is not provided else returns None.
+    """
+    if item.inventory.type in REQUIERE_DURATION_TYPE \
+        and item.duration == ITEM_DURATION_DEFULT_VALUE:
+        return MSG_E_ITEM_DURATION_REQUIRED
 
-def validate_item_security_level(security_level: str):
-    pass
 
-# Below is dictionaries with validation functions for items
-# Each dictionary coresponds to specific inventory type and media
-# Dictionary contains Item fields name as key
+# Below is dictionarie with item validation functions.
+# Dictionary contains Item fields name as keys
 # and function name, against which field should be checked, as value
-VALIDATION_DICT_MANDATORY_FIELDS = {
-    'item_series_code': validate_item_series_code,
-    'item_number': validate_item_number, 
-    'item_title': validate_item_title,
-    'item_start_date': validate_item_start_date,
-    'item_end_date': validate_item_end_date,
-    'item_size': validate_item_size,
-    'item_unit_of_measur': validate_item_unit_of_measur,
-    'item_notes': validate_item_notes,
-    'item_language': validate_item_language,
-    'item_restriction': validate_item_restriction,
-    'item_security_level': validate_item_security_level
-}
-
-VALIDATION_DICT_OPTIONAL_FIELDS = {
-    'related_item': validate_related_item,
-    'item_sistematisation': validate_item_sistematisation
-}
-
-VALIDATION_DICT_E_FOTO_FONO_VIDEO_FIELDS = {
+VALIDATION_DICT_FIELDS_FUNCTION = {
+    'number': validate_item_number,
+    'start_date': validate_item_date,
+    'size': validate_item_size,
+    'unit_of_measur': validate_item_unit_of_measure,
+    'language': validate_item_language,
+    'restriction_note': validate_item_restriction_note,
     'item_annotation': validate_item_annotation,
-    'item_physical_description': validate_item_physical_description,
+    'color': validate_item_color,
+    'format': validate_item_format,
+    'resolution': validate_item_resolution,
+    'duration': validate_item_duration,
 }
 
-def item_validation_text(item: dict, inventory: Inventory):
-    """Validate text documents with paper media"""
+def item_validators(item) -> None:
+    """This function collects all vallidation errors.
+    
+    Args:
+        item: Item instance to be validated.
+        
+    Raises:
+        ValidationError: If item is with errors."""
 
     # Create dictionary for errors
     item_errors = {}
 
     # Run through all validation functions.
-    for field, function in VALIDATION_DICT_MANDATORY_FIELDS.items():
-        field_value = item.get(field, NO_VALUE)
+    for field, function in VALIDATION_DICT_FIELDS_FUNCTION.items():
+        field_value = getattr(item, field, NO_VALUE)
+        print(field)
 
-        # Validate if value exists.
+        # There is no value
         if field_value == NO_VALUE:
             continue
-        
-        # Get validation function parameters
-        sig = signature(function)
 
-        # Validate value against validation function.
-        if len(sig.parameters) > 1:
-            # Pass inventory as second parameter if number of mandatory parameters
-            # of the function is more than one.
-            result = function(field_value, inventory)
-        else:
-            result = function(field_value)
+        result = function(item)
         
-        if result is not None:
+        if result:
             # Add error message
             item_errors[field] = result
     
+    # Check if item has errors.
     if item_errors:
-        return False, item_errors
+        raise ValidationError(item_errors)
     
-    return True, item
-
-def item_validation_e_text(items: list):
-    pass
-
-def item_validation_e_foto(items: list):
-    pass
-
-def item_validation_e_fono(items: list):
-    pass
-
-def item_validation_e_video(items: list):
-    pass
-
-def item_validation_e_database(items: list):
-    pass
-
-TYPE_SELECTION_DICT = {
-    'tekstuāls': item_validation_e_text,
-    'foto': item_validation_e_foto,
-    'skaņas': item_validation_e_fono,
-    'video': item_validation_e_video,
-    'datubāze': item_validation_e_database
-}
-
-def select_item_validator(item: dict, inventory: Inventory):
-    """Select correct item validator.
-    
-    Selects item validator according to inventory type and media"""
-
-    # Get type and media of inventory list
-    inventory_type = inventory.type
-    inventory_media = inventory.electronic
-
-    #if not inventory_media:
-    validated, result = item_validation_text(item, inventory)
-    
-    # else:
-    #     validation_function = TYPE_SELECTION_DICT[inventory_type]
-    #     validated, result = validation_function(items)
-    
-    if not validated:
-        raise ValueError(result)
-
-    return result
