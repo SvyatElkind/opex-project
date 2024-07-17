4bf3e8d654723cd921d7193d647f1c8a538bb396
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-07-17 15:25:56 +0300

    Update serializer

--- project/serializers.py
+++ project/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
-class ProjectSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=50)
-    created_at = serializers.DateTimeField()
-    validated = serializers.BooleanField()
+from project.models import Project
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
