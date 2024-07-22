176dddd1e81e3b6ad32905427575af47fecc6f4b
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-07-22 13:36:21 +0300

    Update serializers and views

--- project/views.py
+++ project/views.py
@@ -1,29 +1,75 @@
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
-from project.serializers import ProjectSerializer
+from project.helpers.constants import MSG_E_NO_PROJECT
+from project.serializers import (
+    SpecificProjectSerializer,
+    ProjectSerializer
+)
 
 from .models import Project
 
+
+class SpecificProjectAPIView(APIView):
+    """API view to get all data of specific project"""
+
+    def get(self, request, project_id):
+        """Get all data related to specific project."""
+        project = get_object_or_404(Project, id=project_id)
+        try:
+            data = project.get_project_data()
+        except Exception as ex:
+            return Response(data=ex.args[0], status=status.HTTP_400_BAD_REQUEST)
+        # serialize project data
+        serializer = SpecificProjectSerializer(data)
+        return Response(data = serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, project_id):
+        """Update existing project."""
+        project = get_object_or_404(Project, id=project_id)
+        serializer = ProjectSerializer(project, data=request.data, partial=True)
+        
+        if serializer.is_valid():
+            try:
+                serializer.save()
+            except ValidationError as ex:
+                return Response(data=ex.args[0], status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+        
+        # Return validation errors
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, project_id):
+        """Delete specific project and all related data."""
+        project = get_object_or_404(Project, id=project_id)
+        project.delete_project()
+        return Response(data={"success": "Projekts ir dzēsts"}, status=status.HTTP_200_OK)
+
 class ProjectAPIView(APIView):
-    """API View for requests related to all projects"""
+    """API view for post, put and delete methods."""
     serializer_class = ProjectSerializer
 
     def get(self, request):
-        """Get all projects"""
-        projects = Project.get_all_projects()
+        """Get all projects."""
+        projects = Project.objects.all()
         if not projects:
-            return Response(data = {"error": "There is no projects"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data = {"project": None}, status=status.HTTP_204_NO_CONTENT)
         serializer = self.serializer_class(projects, many=True)
         return Response(data = serializer.data, status=status.HTTP_200_OK)
 
-class SpecificProjectAPIView(APIView):
-    """API View for requests related to specific project"""
-    serializer_class= ProjectSerializer
-
-    def get(self, request, project_id):
-        data = Project.get_project_related_data(project_id)
-        if not data:
-            return Response(data = {"error": "There is no projects"}, status=status.HTTP_400_BAD_REQUEST)
-        return Response(data = data, status=status.HTTP_200_OK)
+    def post(self, request):
+        """View for create new project."""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            try:
+                serializer.save()
+            except ValidationError as ex:
+                 return Response(data=ex.args[0], status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
