157fceab30477320e9d9d4788cff8ccdf9cac746
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-07-24 15:24:53 +0300

    Update url name

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from .views import SpecificProjectAPIView, ProjectAPIView
+from .views import (
+    SpecificProjectAPIView,
+    ProjectAPIView,
+    AddDataToProjectAPIView,
+)
 
 
 app_name = 'projects'
@@ -8,4 +12,5 @@ app_name = 'projects'
 urlpatterns =  [
     path('project/', ProjectAPIView.as_view()),
     path('project/<int:project_id>/', SpecificProjectAPIView.as_view()),
+    path('project/<int:project_id>/add_report/', AddDataToProjectAPIView.as_view()),
 ]
