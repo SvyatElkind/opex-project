e90e1ad97442f051f0164a15ff1095b12bf4490b
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-07-30 13:34:13 +0300

    Add constants

--- institutions/helpers/constants.py
+++ institutions/helpers/constants.py
@@ -2,10 +2,14 @@
 
 # Institution class CharFields max length.
 INSTITUTION_NAME_LENGTH = 500
+REG_NR_LENGTH = 15
 CREATOR_LENGTH = 30
 CREATOR_POSITION_LENGTH = 200
 SIGNER_LENGTH = 30
 SIGHER_POSITION_LENGTH = 200
+
+# Regular expression
+REGEX_REG_NR = r'^\d{1,15}$'
 
 # Error messages.
 MSG_E_INSTITUTION_NAME_LENGTH = f'Institūcijas nosaukums nevar būt garāks par {INSTITUTION_NAME_LENGTH} burtiem'
@@ -17,6 +21,7 @@ MSG_E_INSTITUTION_NAME_UNEQUE = 'Institūcija ar šo nosaukumu jau eksistē.'
 MSG_E_REG_NR_UNIQUE = 'Institūcija ar šo rģistrācijas numuru jau eksistē.'
 MSG_E_EMPTY_FIELDS = 'Nav aizpildīti lauki: {}.'
 MSG_E_REDUNDANT_FIELDS = 'Ir saņemti neparedzētie lauki.'
+MSG_E_REG_NR = 'Reģistrācijas numurs var saturēt tikai ciparus un nevar būt garāks par 15 simboliem.'
 
 # Serializers
 FIELDS = 'fields'
