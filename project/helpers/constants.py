9e6ef7a3b118bdebd22e36cb8a6d0b59354ad5d4
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-08-27 15:37:09 +0300

    Add constant

--- project/helpers/constants.py
+++ project/helpers/constants.py
@@ -12,6 +12,7 @@ REGEX_PROJECT_FOLDER = r'^(?:[a-zA-Z]:\\|/)(?:[^\\/:*?"<>|\r\n]*(?:[\\/:][^\\/:*
 # Success messages:
 MSG_PROJECT_DELETED = 'Projekts ir dzēsts.'
 MSG_REPORT_IMPORTED = "Atskaite ir importēta"
+MSG_NO_STRUCTURE = 'Projektam nav sagatavotas struktūras.'
 
 # Response keys:
 FOLDER = 'folder'
