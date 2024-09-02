ebfaedc4be7d9072d016849ba9c343b01b6664cf
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-09-02 14:42:40 +0300

    Add validators

--- inventories/helpers/validators.py
+++ inventories/helpers/validators.py
@@ -1,10 +1,11 @@
 """Module contains validation functions for Inventory class"""
 
-from datetime import date
+
+from django.db.models import Max
 from rest_framework import serializers
 
 from fonds.models import Fond
-from helpers.constants import VVAIS_STORAGE_TERM_LIST, VVAIS_TYPE_LIST
+from helpers.constants import MSG_E_OBJECT_NUMBER, VVAIS_STORAGE_TERM_LIST, VVAIS_TYPE_LIST
 from inventories.helpers.constants import (
     MSG_E_FOND_DOES_NOT_EXIST,
     MSG_E_INVENTORY_POSTFIX_LENGTH,
@@ -57,8 +58,31 @@ def validate_storage_term(storage_term: str) -> None:
         raise ValidationError(MSG_E_INVENTORY_STORAGE_TERM)
 
 
+def validate_inventory_number(instance):
+    """Checks if object with given number can be created.
+    
+    Args:
+        instance: Model instance.
+    
+    Raises:
+        ValidationError if any errors appears during validation.
+    """
+    # Get last objects number.
+    last_number = type(instance).objects.filter(fond_id=instance.fond.id).aggregate(Max('number'))['number__max']     
+    if not isinstance(last_number, int):
+        if not instance.number == 1:
+            raise ValidationError(MSG_E_OBJECT_NUMBER.format(type(instance).__name__))
+
+    elif not last_number + 1 == instance.number:
+        raise ValidationError(MSG_E_OBJECT_NUMBER.format(type(instance).__name__))
+    
+
+# Below are serializer level validators
+# --------------------------------------
 def validate_if_fond_exists(fond_id: str) -> None:
     """Validate if fond_id is provided and fond exists.
+    
+    This is serializer validation.
     
     Args:
         fond_id: Fond id number.
@@ -78,8 +102,10 @@ def validate_if_fond_exists(fond_id: str) -> None:
         raise serializers.ValidationError(MSG_E_FOND_DOES_NOT_EXIST)
     
 
-def validate_inventory_date(start_date: date, end_date: date) -> None:
-    """Validate start and end date year.
+def validate_inventory_date(start_date, end_date) -> None:
+    """Validate year of start and end date.
+
+    This is serializer validation.
     
     Args:
         start_date: Inventory start date.
@@ -88,7 +114,7 @@ def validate_inventory_date(start_date: date, end_date: date) -> None:
     Raises:
         ValidationError: If year of start date is 
             bigger then year of end date.
-    """
-    if start_date.year > end_date.year:
+    """    
+    if start_date > end_date:
         raise serializers.ValidationError(MSG_E_WRONG_DATE)
     
