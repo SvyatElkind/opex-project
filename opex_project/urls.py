ec53ed262081acc4276f450b43832e209acd166b
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-09-19 14:12:41 +0300

    add urls

--- opex_project/urls.py
+++ opex_project/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('project.urls')),
     path('api/v1/', include('institutions.urls')),
-    path('api/v1/', include('inventories.urls'))
+    path('api/v1/', include('inventories.urls')),
+    path('api/v1/', include('items.urls')),
 ]
