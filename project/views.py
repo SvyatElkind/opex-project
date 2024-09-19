2e11a4977e9bcc16cf33c45b5f98bc8ba07bfbac
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2024-09-19 13:12:06 +0300

    Update project views

--- project/views.py
+++ project/views.py
@@ -4,20 +4,16 @@
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.parsers import FileUploadParser
-from rest_framework import status
 
 from helpers.constants import ERROR, MSG_E_UNPREDICTIBLE_ERROR_OCCURED, SUCCESS
 from helpers.local_imports import import_report_file
 from helpers.mixins import ResponseMixin
 from project.helpers.constants import (
-    MSG_E_REPORT_ALREADY_EXIST,
     MSG_PROJECT_DELETED,
     MSG_REPORT_IMPORTED,
-    PROJECT,
-    MSG_E_NO_REPORT,
+    PROJECT
 )
 from project.serializers import (
     SpecificProjectSerializer,
@@ -29,7 +25,7 @@ from .models import Project
 
 
 class SpecificProjectAPIView(ResponseMixin, APIView):
-    """API view to get all data of specific project"""
+    """API view to get all data of specific project."""
 
     def get(self, request, project_id):
         """Get all data related to specific project."""
@@ -42,6 +38,7 @@ class SpecificProjectAPIView(ResponseMixin, APIView):
             return self.response({ERROR: MSG_E_UNPREDICTIBLE_ERROR_OCCURED}, 400)
         
         serializer = SpecificProjectSerializer(data)
+
         return self.response(serializer.data, 200)
 
     def put(self, request, project_id):
@@ -60,7 +57,6 @@ class SpecificProjectAPIView(ResponseMixin, APIView):
     
             return self.response(serializer.data, 200)
         
-        # Return validation errors
         return self.response(serializer.errors, 400)
 
     def delete(self, request, project_id):
@@ -75,7 +71,7 @@ class SpecificProjectAPIView(ResponseMixin, APIView):
         return self.response({SUCCESS: MSG_PROJECT_DELETED}, 200)
 
 class ProjectAPIView(ResponseMixin, APIView):
-    """API view for post and delete methods."""
+    """API view for creating new project and get the list of existing projects."""
     serializer_class = ProjectSerializer
 
     def get(self, request):
@@ -86,6 +82,7 @@ class ProjectAPIView(ResponseMixin, APIView):
             return self.response({PROJECT: None}, 204)
         
         serializer = self.serializer_class(projects, many=True)
+
         return self.response(serializer.data, 200)
 
     def post(self, request):
@@ -113,26 +110,20 @@ class AddReportToProjectAPIView(ResponseMixin, APIView):
 
     @csrf_exempt
     def post(self, request, project_id):
-        """Add report from VVAIS to the project."""
+        """Add report from VVAIS."""
         project = get_object_or_404(Project, id=project_id)
       
-        serializer = VVAISReportFileSerializer(data={'file': request.data['file']})
+        serializer = self.serializer_class(data={'file': request.data['file']})
 
         if serializer.is_valid():
             try:
                 import_report_file(request.data['file'], project)
             except ValidationError as ex:
                 return self.response(ex.args[0], 400)
+            except Exception as ex:
+                return self.response({ERROR: MSG_E_UNPREDICTIBLE_ERROR_OCCURED}, 400)
                
             return self.response({SUCCESS: MSG_REPORT_IMPORTED}, 200)
 
         return self.response(serializer.errors, 400)
     
-
-class ReportAPIView(APIView):
-    """API view for report indicator."""
-    def put(self, request, project_id):
-        project = Project.objects.get(id=project_id)
-        project.change_report_status()
-        
-        return Response(data={'success': 'Report added'}, status=status.HTTP_200_OK)
