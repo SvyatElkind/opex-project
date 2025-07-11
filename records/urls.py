884978b7749fb462a152345eb52dff608b0ac8de
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2025-07-11 15:49:42 +0300

    Update urls

--- records/urls.py
+++ records/urls.py
@@ -3,15 +3,25 @@ from django.urls import path
 from .views import (
     AddMetadataAPIView,
     AddRecordAPIView,
+    FileDeleteAPIView,
+    MediaFileUploadAPIView,
+    MediaRecordAPIView,
     MetadataAPIView,
-    RecordAPIView
+    MultipleFileUploadAPIView,
+    RecordAPIView,
+
 )
 
 app_name = 'records'
 
 urlpatterns =  [
     path('project/<int:project_id>/record/', AddRecordAPIView.as_view()),
+    path('project/<int:project_id>/media_record/', MediaFileUploadAPIView.as_view()),
     path('project/<int:project_id>/record/<int:record_id>/', RecordAPIView.as_view()),
+    path('project/<int:project_id>/media_record/<int:record_id>/', MediaRecordAPIView.as_view()),
     path('project/<int:project_id>/record/<int:record_id>/additional_metadata/', AddMetadataAPIView.as_view()),
     path('project/<int:project_id>/record/<int:record_id>/additional_metadata/methods/', MetadataAPIView.as_view()),
+    path('project/<int:project_id>/record/<int:record_id>/multiple_files/', MultipleFileUploadAPIView.as_view()),
+    path('project/<int:project_id>/file/<int:file_id>/', FileDeleteAPIView.as_view()),
+    
 ]
