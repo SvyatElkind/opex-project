ca27bb7641316ca500b90b3f0b3887316e90abca
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2025-07-17 11:36:00 +0300

    Update views

--- records/views.py
+++ records/views.py
@@ -114,28 +114,19 @@ class RecordAPIView(ProjectRelationMixin, ResponseMixin, APIView):
         except Exception as ex:
             return self.response(ex.args[0], 400)
 
-        return self.response({SUCCESS: 'Record deleted successfully'}, 200)
+        return self.response({SUCCESS: MSG_RECORD_DELETED}, 200)
 
 
 class AddMetadataAPIView(ProjectRelationMixin, ResponseMixin, APIView):
     """
     API view to create Action, Addressee, Visa, or ReadStatus instance
     using add_metadata() in records.models.
-
     """
-    serializer_map = {
-        'action': ActionSerializer,
-        'addressee': AddresseeSerializer,
-        'visa': VisaSerializer,
-        'read_status': ReadStatusSerializer
-    }
-
-
     def post(self, request, project_id, record_id):
         """Create metadata instance for a record."""
         # Get metadata class name.
         model_class_name = request.query_params.get('class')
-        serializer_class = self.serializer_map.get(model_class_name)
+        serializer_class = ADDITIONAIL_METADATA_MAP.get(model_class_name)
         if not serializer_class:
             return self.response({ERROR: MSG_E_UNKNOWN_CLASS.format(model_class_name)}, 400)
 
@@ -162,30 +153,17 @@ class AddMetadataAPIView(ProjectRelationMixin, ResponseMixin, APIView):
 
 class MetadataAPIView(ProjectRelationMixin, ResponseMixin, APIView):
     """API view for specific metadata."""
-    serializer_map = {
-        'action': UpdateActionSerializer,
-        'addressee': UpdateAddresseeSerializer,
-        'visa': UpdateVisaSerializer,
-        'read_status': UpdateReadStatusSerializer
-    }
-
-    model_class_map = {
-            'action': Action,
-            'addressee': Addressee,
-            'visa': Visa,
-            'read_status': ReadStatus
-            }
 
     def put(self, request, project_id, record_id):
         """Update metadata instance."""
         model_class_name = request.query_params.get('class')
-        serializer_class = self.serializer_map.get(model_class_name)
+        serializer_class = UPDATE_ADDITIONAL_METADATA_MAP.get(model_class_name)
         if not serializer_class:
             return self.response({ERROR: MSG_E_UNKNOWN_CLASS.format(model_class_name)}, 400)
 
         metadata_id = request.query_params.get('id')
         try:
-            model_class = self.model_class_map.get(model_class_name)
+            model_class = METADATA_CLASS_MAP.get(model_class_name)
             if not model_class:
                 return self.response({ERROR: MSG_E_UNKNOWN_CLASS.format(model_class_name)}, 400)
             metadata_instance = model_class.objects.get(id=metadata_id)
@@ -212,7 +190,7 @@ class MetadataAPIView(ProjectRelationMixin, ResponseMixin, APIView):
         model_class_name = request.query_params.get('class')
         metadata_id = request.query_params.get('id')
         try:
-            model_class = self.model_class_map.get(model_class_name)
+            model_class = METADATA_CLASS_MAP.get(model_class_name)
             if not model_class:
                 return self.response({ERROR: MSG_E_UNKNOWN_CLASS.format(model_class_name)}, 400)
             metadata_instance = model_class.objects.get(id=metadata_id)
@@ -248,84 +226,140 @@ class MultipleFileUploadAPIView(ProjectRelationMixin, ResponseMixin, APIView):
         project = Project.objects.get(id=project_id)
         project_folder = project.folder
 
-        File.add_files(files, record, project_folder)
+        try:
+            File.add_files(files, record, project_folder)
+        except Exception as ex:
+            return self.response({ERROR: ex.args[0]}, 400)
 
         return self.response({SUCCESS: MSG_FILES_UPLOADED}, 201)
 
+
 class FileDeleteAPIView(ResponseMixin, APIView):
-    """API view for deleting a file from a record."""
+    """API view for deleting a file from a record.
+    
+    Only text inventory related files can be deleted."""
 
     def delete(self, request, project_id, file_id):
         """Delete a file from the record."""
-
+        
+        # Validate if there is a file and if it is related to inventory with type 'Tekstuāls'.
+        # Delition of media files heppens during media record deletion.
         file = File.objects.filter(id=file_id).first()
         if not file:
-            return self.response({ERROR: 'File not found.'}, 404)
+            return self.response({ERROR: MSG_E_NO_FILE.format(file_id)}, 400)
+        if not file.record:
+            return self.response({ERROR: MSG_E_IS_NOT_TEXT_FILE}, 400)
 
         try:
             file.delete()
-        except FileNotFoundError:
-            return self.response({ERROR: f'File "{file.id}" not found.'}, 404)
         except Exception as ex:
-            return self.response({ERROR: str(ex)}, 400)
+            return self.response(ex.args[0], 400)
 
-        return self.response({SUCCESS: f'File "{file.original_name}" deleted successfully.'}, 200)
+        return self.response({SUCCESS: MSG_FILE_DELETED}, 200)
     
-class MediaFileUploadAPIView(ProjectRelationMixin, ResponseMixin, APIView):
-    """API view for creating media record and uploading media file."""
+
+class AddMediaRecordAPIView(ProjectRelationMixin, ResponseMixin, APIView):
+    """API view for creating media record.
+    
+    Media record is always created with a file upload."""
     parser_classes = [MultiPartParser]
 
-    media_type_map = {
-        'Foto': PhotoRecord,
-        'Video': VideoRecord,
-        'Audio': AudioRecord
-    }
-
     def post(self, request, project_id):
-        """Handle media file uploads."""
+        """Handles creation of media record."""
+
+        # Get item instance to assign media record to it.
         item_id = request.query_params.get('item_id')
-        item = Item.objects.get(id=item_id)
-        if not (item.inventory.type in INVENTORY_MEDIA_TYPE and item.inventory.electronic):
-            return self.response({ERROR: 'GV tips nav foto, video, skaņas un/vai elektroniskā formā '}, 400)
+        if not item_id:
+            return self.response({ERROR: MSG_E_NO_INTEM_ID}, 400)
         
-        if item.inventory.type == 'Foto':
-            if item.photo_records.exists():
-                return self.response({ERROR: 'Item already has a photo record.'}, 400)
-        # elif item.inventory.type == 'Video':
-        #     if item.video_records.exists():
-        #         return self.response({ERROR: 'Item already has a video record.'}, 400)
+        item = Item.objects.filter(id=item_id).first()
+        if not item:
+            return self.response({ERROR: MSG_E_NO_ITEM.format(item_id)}, 400)
+        
+        # Validations
+        try:
+            item_type = validate_if_is_media_type(item)
+            validate_if_record_exists(item)
+        except ValidationError as ex:
+            return self.response(ex.args[0], 400)
 
         # Expects files in 'files' key of request.FILES.
         # Only one file is allowed to be uploaded at a time.
-        files = request.FILES.getlist('files')
-        if not files:
+        file = request.FILES.getlist(FILES)
+        if not file:
             return self.response({ERROR: MSG_E_NO_FILES_PROVIDED}, 400)
-        if len(files) > 1:
-            return self.response({ERROR: 'Only one file can be uploaded'}, 400)
+        if len(file) > 1:
+            return self.response({ERROR: MSG_E_NO_MULTIPLE_FILES_ALLOWED}, 400)
         
         # Get media record class 
-        media_class = self.media_type_map.get(item.inventory.type)
-        if not media_class:
-            return self.response({ERROR: 'GV tips nav foto, video, skaņas'}, 400)
+        media_class = MEDIA_CLASS_MAP.get(item_type)
 
         project = Project.objects.get(id=project_id)
         project_folder = project.folder
 
         # Create media record and add file to it
         try:
-            media_class.add_record(files[0], project_folder, item)
+            media_record = media_class.add_record(file, project_folder, item)
+        except ValidationError as ex:
+            return self.response(ex.args[0], 400)
+        
+        # Get proper serializer class for media record
+        serializer_class = MEDIA_RECORD_SERIALIZER_MAP.get(item_type)
+        serializer = serializer_class(media_record)
+
+        return self.response(serializer.data, 201)
+
+
+class MediaRecordAPIView(ProjectRelationMixin, ResponseMixin, APIView):
+    """API View for specific media record."""
+
+    def put(self, request, project_id, record_id):
+        """Update media record."""
+
+        record_type = request.query_params.get('type')
+        if not record_type:
+            return self.response({ERROR: MSG_E_NO_TYPE_PROVIDED}, 400)
+        
+        record_class = MEDIA_CLASS_MAP.get(record_type)
+        if not record_class:
+            return self.response({ERROR: MSG_E_UNKNOWN_CLASS}, 400)
+        
+        try:
+            media_record = self.get_validated_object(project_id, record_class, record_id)        
         except ValidationError as ex:
             return self.response(ex.args[0], 400)
 
-        return self.response({SUCCESS: MSG_FILES_UPLOADED}, 201)
+        serializer_class = MEDIA_RECORD_SERIALIZER_MAP.get(record_type)
 
-class MediaRecordAPIView(ProjectRelationMixin, ResponseMixin, APIView):
-    
+        serializer = serializer_class(media_record, data=request.data)
+
+        if serializer.is_valid():
+            try:
+                serializer.save()
+            except ValidationError as ex:
+                 return self.response(ex.args[0], 400)
+            except Exception as ex:
+                return self.response({ERROR: MSG_E_UNPREDICTIBLE_ERROR_OCCURED}, 400)
+
+            return self.response(serializer.data, 200)
+
+        return self.response(serializer.errors, 400)    
 
     def delete(self, request, project_id, record_id):
-        """Delete record."""
+        """Delete record.
+        
+        Only media records can be deleted through this view."""
+
+        record_type = request.query_params.get('type')
+        if not record_type:
+            return self.response({ERROR: MSG_E_NO_TYPE_PROVIDED}, 400)
+        
+        record_class = MEDIA_CLASS_MAP.get(record_type)
+        if not record_class:
+            return self.response({ERROR: MSG_E_UNKNOWN_CLASS}, 400)
+        
         try:
-            record = self.get_validated_object(project_id, PhotoRecord, record_id)
+            record = self.get_validated_object(project_id, record_class, record_id)
         except ValidationError as ex:
             return self.response(ex.args[0], 400)
         
@@ -334,4 +368,4 @@ class MediaRecordAPIView(ProjectRelationMixin, ResponseMixin, APIView):
         except Exception as ex:
             return self.response(ex.args[0], 400)
 
-        return self.response({SUCCESS: 'Record deleted successfully'}, 200)
+        return self.response({SUCCESS: MSG_RECORD_DELETED}, 200)
