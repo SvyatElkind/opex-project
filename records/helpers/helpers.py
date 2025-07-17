f8fb3da86f634523311a6fc1d352f9c7689a61b9
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2025-07-17 11:36:27 +0300

    Update

--- records/helpers/helpers.py
+++ records/helpers/helpers.py
@@ -1,5 +1,2 @@
 def get_metadata_from_file(file_instance, media_record):
-    media_record.validated = True
-    media_record.color = "Melnbalta"
-    media_record.save()
-    return True
+    return media_record
