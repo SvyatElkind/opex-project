e429c4fa99c83fc33d9b0e728c2982e2cb37b4a6
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2025-07-17 11:32:06 +0300

    rearrange urls

--- records/urls.py
+++ records/urls.py
@@ -16,12 +16,13 @@ app_name = 'records'
 
 urlpatterns =  [
     path('project/<int:project_id>/record/', AddRecordAPIView.as_view()),
-    path('project/<int:project_id>/media_record/', MediaFileUploadAPIView.as_view()),
     path('project/<int:project_id>/record/<int:record_id>/', RecordAPIView.as_view()),
-    path('project/<int:project_id>/media_record/<int:record_id>/', MediaRecordAPIView.as_view()),
     path('project/<int:project_id>/record/<int:record_id>/additional_metadata/', AddMetadataAPIView.as_view()),
     path('project/<int:project_id>/record/<int:record_id>/additional_metadata/methods/', MetadataAPIView.as_view()),
-    path('project/<int:project_id>/record/<int:record_id>/multiple_files/', MultipleFileUploadAPIView.as_view()),
+   
+    path('project/<int:project_id>/record/<int:record_id>/multiple_files/', MultipleFileUploadAPIView.as_view()), 
     path('project/<int:project_id>/file/<int:file_id>/', FileDeleteAPIView.as_view()),
-    
+
+    path('project/<int:project_id>/media_record/', AddMediaRecordAPIView.as_view()),
+    path('project/<int:project_id>/media_record/<int:record_id>/', MediaRecordAPIView.as_view()),
 ]
