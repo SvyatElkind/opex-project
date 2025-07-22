69540b47f64ddcb47df4ec006b9bfbccc6657303
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2025-07-22 10:41:20 +0300

    Add logger

--- items/views.py
+++ items/views.py
@@ -1,13 +1,17 @@
 """Module contains api views for item app."""
+import logging
 
 from django.core.exceptions import ValidationError
 from rest_framework.views import APIView
 
 from helpers.constants import ERROR, MSG_E_UNPREDICTIBLE_ERROR_OCCURED, SUCCESS
-
 from helpers.mixins import ProjectRelationMixin, ResponseMixin
 from items.models import Item
 from items.serializers import ItemSerializer, UpdateItemSerializer
+
+
+logger = logging.getLogger(__name__)
+
 
 class AddItemAPIView(ResponseMixin, APIView):
     """API view creating item."""
@@ -23,12 +27,15 @@ class AddItemAPIView(ResponseMixin, APIView):
             try:
                 serializer.save()
             except ValidationError as ex:
+                logger.error(f"{self.__class__.__name__}: {ex.args[0]}")
                 return self.response(ex.args[0], 400)
             except Exception as ex:
+                logger.error(f"{self.__class__.__name__}: {ex}")
                 return self.response({ERROR: MSG_E_UNPREDICTIBLE_ERROR_OCCURED}, 400)
             
             return self.response(serializer.data, 201)
-        
+
+        logger.error(f"{self.__class__.__name__}: {serializer.errors}")
         return self.response(serializer.errors, 400)
 
 
@@ -41,6 +48,7 @@ class ItemAPIView(ProjectRelationMixin, ResponseMixin, APIView):
         try:
             item = self.get_validated_object(project_id, Item, item_id)        
         except ValidationError as ex:
+            logger.error(f"{self.__class__.__name__}: {ex.args[0]}")
             return self.response(ex.args[0], 400) 
 
         serializer = self.serializer_class(item,
@@ -51,12 +59,15 @@ class ItemAPIView(ProjectRelationMixin, ResponseMixin, APIView):
             try:
                 serializer.save()
             except ValidationError as ex:
-                 return self.response(ex.args[0], 400)
+                logger.error(f"{self.__class__.__name__}: {ex.args[0]}")
+                return self.response(ex.args[0], 400)
             except Exception as ex:
+                logger.error(f"{self.__class__.__name__}: {ex}")
                 return self.response({ERROR: MSG_E_UNPREDICTIBLE_ERROR_OCCURED}, 400)
             
             return self.response(serializer.data, 200)
         
+        logger.error(f"{self.__class__.__name__}: {serializer.errors}")
         return self.response(serializer.errors, 400)
     
     def delete(self, request, project_id, item_id):
@@ -64,11 +75,13 @@ class ItemAPIView(ProjectRelationMixin, ResponseMixin, APIView):
         try:
             item = self.get_validated_object(project_id, Item, item_id)        
         except ValidationError as ex:
+            logger.error(f"{self.__class__.__name__}: {ex.args[0]}")
             return self.response(ex.args[0], 400)
         
         try:
-            item.delete_item(project_id)
+            item.delete_item()
         except Exception as ex:
+            logger.error(f"{self.__class__.__name__}: {ex}")
             return self.response(ex.args[0], 400)
         
         return self.response({SUCCESS: 'Item deleted'}, 200)
