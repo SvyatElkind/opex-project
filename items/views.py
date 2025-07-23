a44eb18485cfa97d0cc45aaafe07de1095f4519d
Svyat Elkind <svyat.elkind@arhivi.gov.lv>
2025-07-23 10:21:08 +0300

    Update loggers

--- items/views.py
+++ items/views.py
@@ -28,13 +28,13 @@ class AddItemAPIView(ProjectRelationMixin, ResponseMixin, APIView):
         try:
             inventory = self.get_validated_object(project_id, Inventory, inventory_id)
         except ValidationError as ex:
-            logger.error(f"{self.__class__.__name__}: {ex.args[0]}")
+            logger.warning(f"{self.__class__.__name__}: {ex.args[0]}")
             return self.response(ex.args[0], 400)
         except Http404 as ex:
-            logger.error(f"{self.__class__.__name__}: {ex}")
+            logger.warning(f"{self.__class__.__name__}: {ex}")
             return self.response({ERROR: MSG_E_NO_INVENTORY.format(inventory_id)}, 404)
         except Exception as ex:
-            logger.error(f"{self.__class__.__name__}: {ex}")
+            logger.error(f"{self.__class__.__name__}: {ex}", exc_info=True)
             return self.response({ERROR: MSG_E_UNPREDICTIBLE_ERROR_OCCURED}, 400)
 
         serializer = self.serializer_class(data=request.data,
@@ -44,15 +44,15 @@ class AddItemAPIView(ProjectRelationMixin, ResponseMixin, APIView):
             try:
                 serializer.save()
             except ValidationError as ex:
-                logger.error(f"{self.__class__.__name__}: {ex.args[0]}")
+                logger.warning(f"{self.__class__.__name__}: {ex.args[0]}")
                 return self.response(ex.args[0], 400)
             except Exception as ex:
-                logger.error(f"{self.__class__.__name__}: {ex}")
+                logger.error(f"{self.__class__.__name__}: {ex}", exc_info=True)
                 return self.response({ERROR: MSG_E_UNPREDICTIBLE_ERROR_OCCURED}, 400)
             
             return self.response(serializer.data, 201)
 
-        logger.error(f"{self.__class__.__name__}: {serializer.errors}")
+        logger.warning(f"{self.__class__.__name__}: {serializer.errors}")
         return self.response(serializer.errors, 400)
 
 
@@ -65,10 +65,10 @@ class ItemAPIView(ProjectRelationMixin, ResponseMixin, APIView):
         try:
             item = self.get_validated_object(project_id, Item, item_id)        
         except ValidationError as ex:
-            logger.error(f"{self.__class__.__name__}: {ex.args[0]}")
+            logger.warning(f"{self.__class__.__name__}: {ex.args[0]}")
             return self.response(ex.args[0], 400)
         except Exception as ex:
-            logger.error(f"{self.__class__.__name__}: {ex}")
+            logger.error(f"{self.__class__.__name__}: {ex}", exc_info=True)
             return self.response({ERROR: MSG_E_UNPREDICTIBLE_ERROR_OCCURED}, 400)
 
         serializer = self.serializer_class(item,
@@ -79,15 +79,15 @@ class ItemAPIView(ProjectRelationMixin, ResponseMixin, APIView):
             try:
                 serializer.save()
             except ValidationError as ex:
-                logger.error(f"{self.__class__.__name__}: {ex.args[0]}")
+                logger.warning(f"{self.__class__.__name__}: {ex.args[0]}")
                 return self.response(ex.args[0], 400)
             except Exception as ex:
-                logger.error(f"{self.__class__.__name__}: {ex}")
+                logger.error(f"{self.__class__.__name__}: {ex}", exc_info=True)
                 return self.response({ERROR: MSG_E_UNPREDICTIBLE_ERROR_OCCURED}, 400)
             
             return self.response(serializer.data, 200)
         
-        logger.error(f"{self.__class__.__name__}: {serializer.errors}")
+        logger.warning(f"{self.__class__.__name__}: {serializer.errors}")
         return self.response(serializer.errors, 400)
     
     def delete(self, request, project_id, item_id):
@@ -95,16 +95,16 @@ class ItemAPIView(ProjectRelationMixin, ResponseMixin, APIView):
         try:
             item = self.get_validated_object(project_id, Item, item_id)        
         except ValidationError as ex:
-            logger.error(f"{self.__class__.__name__}: {ex.args[0]}")
+            logger.warning(f"{self.__class__.__name__}: {ex.args[0]}")
             return self.response(ex.args[0], 400)
         except Exception as ex:
-            logger.error(f"{self.__class__.__name__}: {ex}")
+            logger.error(f"{self.__class__.__name__}: {ex}", exc_info=True)
             return self.response({ERROR: MSG_E_UNPREDICTIBLE_ERROR_OCCURED}, 400)
         
         try:
             item.delete_item()
         except Exception as ex:
-            logger.error(f"{self.__class__.__name__}: {ex}")
+            logger.error(f"{self.__class__.__name__}: {ex}", exc_info=True)
             return self.response(ex.args[0], 400)
         
         return self.response({SUCCESS: 'Item deleted'}, 200)
