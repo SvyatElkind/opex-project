5b31f4385036bbe4aa71b12d23f1f549d2566c60
Svyat Elkind <130361571+SvyatElkind@users.noreply.github.com>
2026-04-22 13:43:45 +0300

    Delete opex_tool_frontend directory

--- opex_tool_frontend/src/Verification/index.js
+++
@@ -1,5 +0,0 @@
-// Verification Module Exports
-export { default as VerificationModal } from './VerificationModal';
-export { default as VerificationSummary } from './VerificationSummary';
-export { default as VerificationTreeView } from './VerificationTreeView';
-export { default as TreeNode } from './TreeNode';
